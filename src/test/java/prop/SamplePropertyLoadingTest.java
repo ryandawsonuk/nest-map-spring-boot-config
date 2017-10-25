07cda160864bc24b8c2fa4a23c378fb7ba814701
Ryan Dawson <ryan.dawson@alfresco.com>
2017-10-25 12:15:23 +0100

    update to boot 2

--- src/test/java/prop/SamplePropertyLoadingTest.java
+++ src/test/java/prop/SamplePropertyLoadingTest.java
@@ -3,14 +3,14 @@ package prop;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = SampleWebApplication.class)
+@SpringBootTest
 public class SamplePropertyLoadingTest {
     @Autowired
     private ActivitiProperty sampleProperty;
